27a0aab89b5d350d5824019a9671e22775b29577
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-05-18 13:12:54 -0500

    Clean files

--- diprow/CorePHP/Libraries/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function autoload($className){
-    $className = str_replace("\\","/",$className);
-    require_once __DIR__."/../../$className.php";
-}
-
-spl_autoload_register('autoload');
